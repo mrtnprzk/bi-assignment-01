dc68abae245a82fe568dc167bbe82c0662e7f36a
Martin Parizek <marti.parizek@gmail.com>
2022-09-23 19:58:17 +0200

    little fix of import hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Reat, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import type { NextPage, GetServerSideProps } from "next";
 import axios, { AxiosError } from "axios";
